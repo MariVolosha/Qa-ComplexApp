82bd1eeaead352b5359d0858e11ffadc27f8de13
MariVolosha <marivolosha@gmail.com>
2021-12-04 12:21:13 +0200

    test#1 added: Verify that title ‘Complex app for testing – QA’ is present on top of the page

--- test_start_page.py
+++ test_start_page.py
@@ -5,12 +5,12 @@ from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestStartPage:
+def random_num():
+    """Generate random number"""
+    return str(random.choice(range(11111, 99999)))
 
-    @staticmethod
-    def random_num(self):
-        """Generate random number"""
-        return str(random.choice(range(11111, 99999)))
+
+class TestStartPage:
 
     def test_start_page(self):
         """Sample test"""
@@ -40,10 +40,10 @@ class TestStartPage:
         driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
         username = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Username']")
         username.clear()
-        username.send_keys(f'Username{self.random_num()}')
+        username.send_keys(f'Username{random_num()}')
         password = driver.find_element(by=By.XPATH, value=".//input[@placeholder='Password']")
         password.clear()
-        password.send_keys(f'pwd{self.random_num()}')
+        password.send_keys(f'pwd{random_num()}')
         button = driver.find_element(by=By.XPATH, value=".//button[text()='Sign In']")
         button.click()
         message = driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger text-center']")
@@ -67,16 +67,16 @@ class TestStartPage:
         # name
         username = driver.find_element(by=By.XPATH, value=".//*[@id='username-register']")
         username.clear()
-        user_text = (f'username{self.random_num()}')
+        user_text = (f'username{random_num()}')
         username.send_keys(user_text)
         # email
         email = driver.find_element(by=By.XPATH, value=".//*[@id='email-register']")
         email.clear()
-        email.send_keys(f'email{self.random_num()}@mail.com')
+        email.send_keys(f'email{random_num()}@mail.com')
         # password
         password = driver.find_element(by=By.XPATH, value=".//*[@id='password-register']")
         password.clear()
-        password.send_keys(f'password{self.random_num()}')
+        password.send_keys(f'password{random_num()}')
         # button
         button = driver.find_element(by=By.XPATH, value=".//button[@type='submit']")
         sleep(2)
@@ -84,3 +84,10 @@ class TestStartPage:
         sleep(2)
         hello = driver.find_element(by=By.XPATH, value=".//H2")
         assert hello.text == f'Hello {user_text}, your feed is empty.'
+
+    def test_check_title(self):
+        """Verify that title ‘Complex app for testing – QA’ is present on top of the page"""
+        driver = webdriver.WebDriver(executable_path="./drivers/chromedriver")
+        driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+        title = driver.find_element(by=By.XPATH, value=".//h4")
+        assert title.is_enabled()
